70a2ac58f1e4ff2ad77db62d8643f26b33e7ffde
Nguyễn Hoàng Anh <hoahoanganh20012001@gmail.com>
2021-12-06 19:12:13 +0700

    add gatsby-soures-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,28 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
-}
+    /* Your site config here */
+    plugins: [
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: 'pages',
+                path: `${__dirname}/src/pages`,
+            },
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: 'posts',
+                path: `${__dirname}/src/posts`,
+            },
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: 'images',
+                path: `${__dirname}/src/images`,
+            },
+        },
+    ],
+};
