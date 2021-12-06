00d06158d5af3cddc5fea0af058d5cf8178b2a1c
Nguyễn Hoàng Anh <hoahoanganh20012001@gmail.com>
2021-12-06 19:29:45 +0700

    add google font

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,5 +42,12 @@ module.exports = {
                 },
             ],
         },
+        {
+            resolve: `gatsby-plugin-google-fonts`,
+            options: {
+                fonts: [`roboto mono`, `muli\:300,400,400i,700, 700i`],
+                display: 'swap',
+            },
+        },
     ],
 };
