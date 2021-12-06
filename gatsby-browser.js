36b6ccd2b760e245eeefb4afc039219277494497
Nguyễn Hoàng Anh <hoahoanganh20012001@gmail.com>
2021-12-06 23:06:16 +0700

    add golbal styles

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,26 @@
 import React from 'react';
-import { ThemeProvider } from 'styled-components';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
 
 import Theme from './src/themes/theme';
 
+const GlobalStyles = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    html, body {
+        height: 100%;
+        font-family: ${(props) => props.theme.fonts.main};
+        background-color: ${(props) => props.theme.colors.light1}
+    }
+
+`;
+
 export const wrapRootElement = ({ element }) => (
-    <ThemeProvider theme={Theme}>{element}</ThemeProvider>
+    <ThemeProvider theme={Theme}>
+        <GlobalStyles />
+        {element}
+    </ThemeProvider>
 );
